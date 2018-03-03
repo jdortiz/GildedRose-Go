023586b5021b72c19d504938e46238b646a17850
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-03-04 00:38:37 +0100

    Refactor unit tests.
    Reuse code among different tests.

--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -3,49 +3,41 @@ package main
 import "testing"
 
 func TestSellInDecreasesOneUnit(t *testing.T) {
-	items := []Item{
-		Item{"Anything", 6, 9},
-	}
-
-	UpdateInventory(items)
-
-	if items[0].sellIn != 5 {
-		t.Errorf("SellIn (expected: %d, actual: %d).", 5, items[0].sellIn)
-	}
+	testSellIn(t, "Anything", 6, 9, 5)
 }
 
 func TestQualityDecreasesOneUnit(t *testing.T) {
-	items := []Item{
-		Item{"Anything", 6, 9},
-	}
-
-	UpdateInventory(items)
-
-	if items[0].quality != 8 {
-		t.Errorf("Quality (expected: %d, actual: %d).", 8, items[0].quality)
-	}
+	testQuality(t, "Anything", 6, 9, 8)
 }
 
 func TestQualityDecreasesTwoUnitsAfterSellDate(t *testing.T) {
-	items := []Item{
-		Item{"Anything", 0, 9},
-	}
-
-	UpdateInventory(items)
-
-	if items[0].quality != 7 {
-		t.Errorf("Quality (expected: %d, actual: %d).", 7, items[0].quality)
-	}
+	testQuality(t, "Anything", 0, 9, 7)
 }
 
 func TestQualityIsNeverNegative(t *testing.T) {
+	testQuality(t, "Anything", 5, 0, 0)
+}
+
+func testSellIn(t *testing.T, itemName string, initiallSellIn int, initialQuality int, expectedSellIn int) {
 	items := []Item{
-		Item{"Anything", 5, 0},
+		Item{itemName, initiallSellIn, initialQuality},
 	}
 
 	UpdateInventory(items)
 
-	if items[0].quality != 0 {
-		t.Errorf("Quality (expected: %d, actual: %d).", 0, items[0].quality)
+	if items[0].sellIn != expectedSellIn {
+		t.Errorf("SellIn (expected: %d, actual: %d).", expectedSellIn, items[0].sellIn)
+	}
+}
+
+func testQuality(t *testing.T, itemName string, initiallSellIn int, initialQuality int, expectedQuality int) {
+	items := []Item{
+		Item{itemName, initiallSellIn, initialQuality},
+	}
+
+	UpdateInventory(items)
+
+	if items[0].quality != expectedQuality {
+		t.Errorf("Quality (expected: %d, actual: %d).", expectedQuality, items[0].quality)
 	}
 }
