e861c33997cba1cc8dbe719ae09734ccf022c8da
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-03-04 02:10:31 +0100

    Complete tests for current domain logic.

--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -18,6 +18,68 @@ func TestQualityIsNeverNegative(t *testing.T) {
 	testQuality(t, "Anything", 5, 0, 0)
 }
 
+func TestAgedBrieSellInDecreasesOneUnit(t *testing.T) {
+	testSellIn(t, "Aged Brie", 5, 10, 4)
+}
+
+func TestAgedBrieQualityIncreasesOneUnit(t *testing.T) {
+	testQuality(t, "Aged Brie", 5, 10, 11)
+}
+
+func TestAgedBrieQualityIncreasesTwoUnitsAfterSellDate(t *testing.T) {
+	testQuality(t, "Aged Brie", 0, 10, 12)
+}
+
+func TestAgedBrieQualityIsNeverMoreThanFifty(t *testing.T) {
+	testQuality(t, "Aged Brie", 5, 50, 50)
+}
+
+func TestSulfurasNeverHasToBeSold(t *testing.T) {
+	testSellIn(t, "Sulfuras, Hand of Ragnaros", 0, 80, 0)
+}
+
+func TestSulfurasQualityNeverDecreases(t *testing.T) {
+	testQuality(t, "Sulfuras, Hand of Ragnaros", 0, 80, 80)
+}
+
+func TestBackstageSellInDecreasesOneUnit(t *testing.T) {
+	testSellIn(t, "Backstage passes to a TAFKAL80ETC concert", 6, 10, 5)
+}
+
+func TestBackstagePassesQualityIncreasesOneUnitIfSellInMoreThanTen(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 11, 20, 21)
+}
+
+func TestBackstagePassesQualityIncreasesTwoUnitsIfSellInLessThanEleven(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 10, 20, 22)
+}
+
+func TestBackstagePassesQualityIncreasesTwoUnitsIfSellInMoreThanFive(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 6, 20, 22)
+}
+
+func TestBackstagePassesQualityIncreasesThreeUnitsIfSellInLessThanSix(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 5, 20, 23)
+}
+
+func TestBackstagePassesQualityIsZeroAfterSellDate(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 0, 20, 0)
+}
+
+func TestBackstagePassesQualityIsNeverMoreThanFiftyWhenIncreasingOne(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 11, 50, 50)
+}
+
+func TestBackstagePassesQualityIsNeverMoreThanFiftyWhenIncreasingTwo(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 7, 49, 50)
+}
+
+func TestBackstagePassesQualityIsNeverMoreThanFiftyWhenIncreasingThree(t *testing.T) {
+	testQuality(t, "Backstage passes to a TAFKAL80ETC concert", 1, 49, 50)
+}
+
+// Auxiliary functions
+
 func testSellIn(t *testing.T, itemName string, initiallSellIn int, initialQuality int, expectedSellIn int) {
 	items := []Item{
 		Item{itemName, initiallSellIn, initialQuality},
