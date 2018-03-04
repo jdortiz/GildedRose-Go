2bbc8ad5183984904042f5543797bf0a53dc0afd
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-03-04 12:50:11 +0100

    Complete refactor for current logic.

--- gilded-rose.go
+++ gilded-rose.go
@@ -29,15 +29,6 @@ func UpdateInventory(items []Item) {
 	for i := 0; i < len(items); i++ {
 
 		switch items[i].name {
-		case "Anything":
-			items[i].changeSellIn(-1)
-			if items[i].sellIn < 0 {
-				items[i].changeQuality(-2)
-			} else {
-				items[i].changeQuality(-1)
-			}
-			items[i].limitQualityToMin(0)
-
 		case "Aged Brie":
 			items[i].changeSellIn(-1)
 			if items[i].sellIn < 0 {
@@ -65,52 +56,13 @@ func UpdateInventory(items []Item) {
 			items[i].limitQualityToMax(50)
 
 		default:
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
+			items[i].changeSellIn(-1)
+			if items[i].sellIn < 0 {
+				items[i].changeQuality(-2)
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
+				items[i].changeQuality(-1)
 			}
-		}
-
+			items[i].limitQualityToMin(0)
 		}
 	}
 }
